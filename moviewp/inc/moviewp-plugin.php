5dc1213194fa00fc3e73292c4b6b276a406daba8
Vincenzo Piromalli <vpiromalli74@gmail.com>
2021-10-18 07:03:21 +0200

    Update moviewp-plugin.php

--- moviewp/inc/moviewp-plugin.php
+++ moviewp/inc/moviewp-plugin.php
@@ -95,7 +95,7 @@ function moviewp_register_required_plugins() {
 			'slug'               => 'Importer', // The plugin slug (typically the folder name).
 			'source'             => 'https://github.com/VincenzoPiromalli/moviewp/blob/main/plugins/Importer.zip?raw=true', // The plugin source.
 			'required'           => false, // If false, the plugin is only 'recommended' instead of required.
-			'version'            => '5.0', // E.g. 1.0.0. If set, the active plugin must be this version or higher. If the plugin version is higher than the plugin version installed, the user will be notified to update the plugin.
+			'version'            => '', // E.g. 1.0.0. If set, the active plugin must be this version or higher. If the plugin version is higher than the plugin version installed, the user will be notified to update the plugin.
 			'force_activation'   => false, // If true, plugin is activated upon theme activation and cannot be deactivated until theme switch.
 			'force_deactivation' => false, // If true, plugin is deactivated upon theme switch, useful for theme-specific plugins.
 			'external_url'       => '', // If set, overrides default API URL and points to an external URL.
